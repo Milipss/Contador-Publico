9c5ea1380f8d6638b8446c8ac5a7604d5b068b77
Milipss <milipaulets@gmail.com>
2025-07-15 11:42:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.querySelectorAll('.materia').forEach(materia => {
+  // cargar estado
+  const nombre = materia.textContent.trim();
+  if (localStorage.getItem(nombre) === 'true') {
+    materia.classList.add('aprobada');
+  }
+
+  // al hacer clic
+  materia.addEventListener('click', () => {
+    materia.classList.toggle('aprobada');
+    const aprobada = materia.classList.contains('aprobada');
+    localStorage.setItem(nombre, aprobada);
+  });
+});
 
